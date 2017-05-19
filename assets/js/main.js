a31027a7a9bd12b441df71cfc10e9b2790a8db98
paulina huañaco <pauli.hu.timm@gmail.com>
2017-05-19 10:28:01 -0400

    Iniciando cometarios, cree uan funcion para las imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+pictures.forEach(function(ele, in){
+	var contenedor = document.getElementById('contPictures');
+	var imagen = document.createElement('div');
+	imagen.setAttribute("class", "pandas");
+	var fotos = document.createElement('img');
+	fotos.src = pictures[in];
+	fotos.id = 'a' + (i+1);
+
+	imagen.appendChild(fotos);
+	contenedor.appendChild(imagen);
+	});
